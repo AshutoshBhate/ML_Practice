849261c00eb9482fc9f6658af4db14d65c9aa24b
CeLViron <ashutoshbhate7@gmail.com>
2026-09-18 01:23:04 +0530

    NC

--- LLM_Learning_Sandbox/Workflow_Patterns_Course/1-introduction/2-structured.py
+++ LLM_Learning_Sandbox/Workflow_Patterns_Course/1-introduction/2-structured.py
@@ -2,6 +2,9 @@ import os
 
 from openai import OpenAI
 from pydantic import BaseModel
+from dotenv import load_dotenv
+
+load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
@@ -10,7 +13,7 @@ class CalendarEvent(BaseModel):
     date: str
     participants: list[str]
 
-completion = client.beta.chat.completions.parse(
+completion = client.chat.completions.parse(
     model="gpt-5-nano",
     messages=[
         {"role": "system", "content": "Extract the event information."},
